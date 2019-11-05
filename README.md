56581da7a1a239472ecd88bbf589e73ec49821f6
musedy <charles09015@gmail.com>
2019-11-06 00:22:28 +0800

    Added user-agent and maintain pep8

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This script is for educational purposes only
 
 ## About the script
 
-Simple webscraper on modelpress website, only works when the images are stored in square components.
+Simple webscraper on modelpress website
--- modelpress_image_scraper.py
+++ modelpress_image_scraper.py
@@ -1,7 +1,6 @@
 '''
 Disclaimer: This script is for educational purposes only
 Simple webscraper on modelpress website
-Only works when the images are stored in square component
 '''
 
 import requests
@@ -10,28 +9,34 @@ import os
 import sys
 from bs4 import BeautifulSoup
 
+
 def get_container(url):
-    r = requests.get(url)
+    headers = {
+        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'}
+    r = requests.get(url, headers=headers)
     if r.status_code == 200:
         soup = BeautifulSoup(r.text, "html.parser")
-        return soup.find_all('figure', class_ = 'square')
+        return soup.find_all('figure', class_='img left')
     else:
         sys.exit("[~] Invalid Response Received.")
 
+
 def main():
-    os.chdir("") # Location of directory
-    html = get_container("") # Modelpress URL here example:'https://mdpr.jp/interview/detail/1882790'
+    os.chdir("")  # Location of directory
+    # Modelpress URL here example:'https://mdpr.jp/interview/detail/1882790'
+    html = get_container("")
     img_url = []
     for a in html:
         if a.img:
             img_url.append(a.img['src'][:99])
     for img in img_url:
         file_name = img.split('/')[-1]
-        print("Downloading file:%s"%file_name)
+        print("Downloading file:%s" % file_name)
         r = requests.get(img, stream=True)
         with open(file_name, 'wb') as f:
             for chunk in r:
                 f.write(chunk)
-    
+
+
 if __name__ == "__main__":
     main()
